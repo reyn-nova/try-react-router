ee458a2d04fc4d309ab1a7a1c9a4357c3e926e39
reynaldpn <reynaldpn@gmail.com>
2020-03-06 14:17:03 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,140 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+require('./App.css')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const React = require('react')
+
+const { Route, Link, Switch, Redirect } = require('react-router-dom')
+
+export default () => {
+  	return (
+		<div>
+			<div
+				className = "nav-bar-container"
+			>
+				<Link
+					className = "nav-bar-link"
+					to = "/"
+				>
+					<h2
+						className = "nav-bar-title"
+					>
+						My Website
+					</h2>
+				</Link>
+
+				<Link
+					className = "nav-bar-link"
+					to = "/messages"
+				>
+					<h4
+						className = "nav-bar-title"
+					>
+						Messages
+					</h4>
+				</Link>
+			</div>
+
+			<div
+				className = "content-container"
+			>
+				<Switch>
+					<Route
+						exact
+						path = "/"
+						component={Home}
+					/>
+
+					<Route
+						path = "/messages"
+						component = {Messages}
+					/>
+
+					<Redirect
+						to = "/"
+					/>
+				</Switch>
+			</div>
+		</div>
+  	)
 }
 
-export default App;
+const Home = () => {
+	return (
+		<p
+			className = "content-container-paragraph"
+		>
+			Home
+		</p>
+	)
+}
+
+const Messages = ({ match }) => {
+	const messages = [
+		{
+			id: 0,
+			message: "Selamat pagi"
+		},
+		{
+			id: 1,
+			message: "Selamat malam"
+		}
+	]
+
+	return (
+		<div>
+			<p
+				className = "content-container-paragraph"
+			>
+				Messages
+			</p>
+
+			<Switch>
+				<Route
+					path = {`${match.url}/:id(\\d+)`}
+					component = {props => (
+						<Message
+							{...props}
+							messages = {messages}	
+						/>
+					)}
+				/>
+
+				<Route
+					path = {match.url}
+					render = {() => <h3>Please select a message</h3>}
+				/>
+			</Switch>
+
+			{
+				messages.map(n => {
+					return (
+						<p
+							key = {n.id}
+						>
+							<Link
+								to = {`${match.url}/${n.id}`}
+							>
+								{n.message}
+							</Link>
+						</p>
+					)
+				})
+			}
+		</div>
+	)
+}
+
+const Message = (props) => {
+	let value = ""
+
+	for(const message of props.messages) {
+		if(Number(message.id) === Number(props.match.params.id)) {
+			value = message.message
+
+			break
+		}
+	}
+
+	return (
+		<h3>Message with ID {props.match.params.id} and value {value}</h3>
+	)
+}
